122841402a3bc1ac5f4cd9819b75ef9c5d080f10
ehsan1222 <s.e.maddahi.ir@gmial.com>
2019-09-06 10:52:11 +0430

    replace exceptions with prints

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import random
 
+from exceptions import *
+
 LIST_OF_WORDS = [
     "apple", "banana", "iran", "java", "python", "laptop", "light", "mars", "country", "china",
     "rocket", "airplane", "superman", "america", "mexico", "dress", "machine", "westworld"
@@ -16,6 +18,34 @@ def create_masked_word(word):
 
 
 def start_game(number_of_suggest=3):
-    word = choose_a_word()
-    masked_word = create_masked_word(word)
+    actual_word = choose_a_word()
+    masked_word = create_masked_word(actual_word)
+    game = {
+        "actual_word": actual_word.lower(),
+        "masked_word": masked_word.lower(),
+        "number_of_suggest": number_of_suggest
+    }
+    return game
 
+
+def guess(game, alphabet):
+    alphabet = alphabet.lower()
+    if not alphabet:
+        raise IllegalWordException
+    if len(alphabet) > 1:
+        raise IllegalWordException
+    if alphabet < 'a' or alphabet > 'z':
+        raise IllegalWordException
+    if alphabet in game['actual_word']:
+        for i in range(len(game['actual_word'])):
+            if game["actual_word"][i] == alphabet:
+                game["masked_word"] = alphabet
+    else:
+        if game["number_of_suggest"] > 1:
+            print(f"Incorrect guess. You can guess it. :)\n")
+            game["number_of_suggest"] -= 1
+        else:
+            raise GameLossException
+
+    if game["actual_word"] == game["masked_word"]:
+        raise GameWonException
