9ec87b918359ad5eb66af2560df267b419c60062
ehsan1222 <s.e.maddahi.ir@gmial.com>
2019-09-06 11:10:49 +0430

    show game process and messages

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from game import *
 
+
+def play(game):
+    print(f"Write Your Guess Character: ")
+    while True:
+        alphabet = input(f"(Remaining Life: {show_life(game)}) Enter a guess ({game['masked_word']}) : ")
+        try:
+            guess(game, alphabet)
+        except IllegalWordException as exp:
+            print(f"Please write a Alphabetic Word")
+            continue
+        except GameLossException as exp:
+            print(f"You are loss. Actual word is ({game['actual_word']})")
+            break
+        except GameWonException as exp:
+            print(f"Oh YES..... You Won!! :)")
+            break
+
+
+def show_life(game):
+    return "|" * game["number_of_suggest"]
+
+
 if __name__ == "__main__":
-    print("welcome to my game:\n")
-    choose_a_word()
+    game = start_game()
+    play(game)
